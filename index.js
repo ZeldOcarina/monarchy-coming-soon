ea505e375717a469b396f9cdf1c4afb78e7d10ae
ZeldOcarina <mattia.r86@gmail.com>
2023-03-01 21:47:41 +0100

    Fix mobile

---
+++ index.js
@@ -0,0 +1,5 @@
+console.log("Hello World");
+
+document.querySelector('video').defaultPlaybackRate = 0.5;
+document.querySelector('video').playbackRate = 0.7;
+document.querySelector('video').play();
